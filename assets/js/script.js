18e52208eb367ab858f5d17a285bf80742194417
Valerie Michalecki <valerie227@gmail.com>
2021-05-21 20:13:51 -0500

    all API functions running properly, broke some out where possible

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 // global variables
 let key = '41fb0d420da1aa1f169bc441322e7bd5';
-let citySearch = document.querySelector('.city-search');
+
 let cityName = document.querySelector('.city-name');
 let date = document.querySelector('.date');
+let icon = document.querySelector('.icon');
 let temp = document.querySelector('.temp');
 let wind = document.querySelector('.wind');
 let humidity = document.querySelector('.humidity');
 let uvIndex = document.querySelector('.uv-index');
 let forecastContainer = document.querySelector('.forecast-container');
+let searchButton = document.querySelector('.search-button');
 
 
 // functions
@@ -16,64 +18,79 @@ let forecastContainer = document.querySelector('.forecast-container');
 // if present, dynamically create buttons with then button label as the city
 // save the city the user searched to local storage, but check local storage for that city first, don't add if already there
 
-function getWeather() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=chicago&units=imperial&appid=2acf688360ef2e4ae9e0ba6153c2285f')
+function getCityName(event) {
+    event.preventDefault();
+    let citySearch = document.querySelector('.city-search').value;
+    getWeather(citySearch);
+}
+
+function getWeather(citySearch) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&units=imperial&appid=${key}`)
         .then(response => response.json())
         .then(data => {
             let nameValue = data['name'];
             let dateValue = new Date(data.dt * 1000);
             let options = { date };
+            let weatherIcon = 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
             let tempValue = data['main']['temp'];
             let windValue = data['wind']['speed'];
             let humidityValue = data['main']['humidity'];
-            console.log(nameValue);
-            console.log(dateValue);
-            let lat = data.coord.lat;
-            let lon = data.coord.lon;
-            getUVI(lat, lon);
-            getFiveDay(nameValue);
             cityName.innerHTML = nameValue;
             date.innerHTML = new Intl.DateTimeFormat('en-US', options).format(dateValue);
+            icon.innerHTML = weatherIcon;
             temp.innerHTML = tempValue;
             wind.innerHTML = windValue;
             humidity.innerHTML = humidityValue;
+            icon.setAttribute('src', weatherIcon);
+            getCoord(citySearch);
         })
+};
 
+function getCoord(citySearch) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&units=imperial&appid=${key}`)
+        .then(response => response.json())
+        .then(data => {
+            let lat = data.coord.lat;
+            let lon = data.coord.lon;
+            getUVI(lat, lon);
+            getFiveDay(lat, lon);
+        })
 };
 
 function getUVI(lat, lon) {
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly,daily,alerts&appid=2acf688360ef2e4ae9e0ba6153c2285f')
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,daily,alerts&appid=${key}`)
         .then(response => response.json())
         .then(data => {
             let uviValue = data['current']['uvi'];
             uvIndex.innerHTML = uviValue;
         })
-}
+};
 
-
-function getFiveDay(nameValue) {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=chicago&appid=2acf688360ef2e4ae9e0ba6153c2285f&cnt=5')
+function getFiveDay(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${key}`)
         .then(response => response.json())
         .then(data => {
             forecastContainer.innerHTML = "";
-            data.list.forEach((lists, index) => {
-                if (index === 0) {
+            data.daily.forEach((day, index) => {
+                if (index === 0 || index > 5) {
                     return;
                 };
-                console.log(lists);
-                let forecastDate = new Date(lists.dt * 1000);
+                let forecastDate = new Date(day.dt * 1000);
                 let options = { date };
-                let forecastIcon = lists.weather[0].icon;
-                let forecastTemp = lists.main.temp;
-                let forecastWind = lists.wind.speed;
-                let forecastHumidity = lists.main.humidity;
+                let forecastIcon = 'https://openweathermap.org/img/w/' + day.weather[0].icon + '.png';
+                let forecastIconDesc = day.weather[0].description || weather[0].main;
+                let forecastTemp = day.temp.day;
+                let forecastWind = day.wind_speed;
+                let forecastHumidity = day.humidity;
                 let forecastCard = document.createElement('div');
                 let cardDate = document.createElement('h3');
-                let cardIcon = document.createElement('p');
+                let cardIcon = document.createElement('img');
                 let cardTemp = document.createElement('p');
                 let cardWind = document.createElement('p');
                 let cardHumidity = document.createElement('p');
                 forecastContainer.append(forecastCard);
+                cardIcon.setAttribute('src', forecastIcon);
+                cardIcon.setAttribute('alt', forecastIconDesc);
                 forecastCard.append(cardDate);
                 forecastCard.append(cardIcon);
                 forecastCard.append(cardTemp);
@@ -84,13 +101,13 @@ function getFiveDay(nameValue) {
                 cardTemp.innerHTML = forecastTemp;
                 cardWind.innerHTML = forecastWind;
                 cardHumidity.innerHTML = forecastHumidity;
-                console.log(forecastIcon);
             })
         })
 };
 
 
-getWeather();
+
+searchButton.addEventListener('click', getCityName);
 
 // events
 // init (check local storage)
